28bad9b01321a900939a7346f515523e29744546
vyomamin3739 <32765938+vyomamin3739@users.noreply.github.com>
2017-10-18 15:43:50 +0530

    TASK#686

---
+++ BOL/CommonFunction.cs
@@ -0,0 +1,35 @@
+﻿using Bsnl.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JmGrove.BOL
+{
+    class CommonFunction
+    {
+        #region Data Members
+
+        public int id { get; set; }
+        public string DesignationType { get; set; }
+        public string DesignationCode { get; set; }
+        #endregion
+
+        public static DataTable GetDesignationType()
+        {
+            database db = new database();
+            db.AddParameter("@QryType", "S");
+            return db.GetDataTable("DesigCode");
+        }
+
+        public static object GetDesignationTypeID(string DesignCode)
+        {
+            database db = new database();
+            db.AddParameter("@DesignationCode", DesignCode);
+            db.AddParameter("@QryType", "G");
+            return db.GetDataValue("DesigCode");
+        }
+    }
+}
---
+++ validation/VD.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Bsnl.Validation
+{
+    class VD
+    {
+        public static bool IsNull(TextBox txt)
+        {
+            bool chk = false;
+            if (txt.Text.Trim().Length == 0)
+            {
+                MessageBox.Show(txt.Tag + " should not be null..!!", "Check", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txt.Focus();
+                chk = true;
+            }
+            return chk;
+        }
+        public static bool IsSelect(ComboBox cmb)
+        {
+            bool chk = true;
+            if (cmb.SelectedIndex == 0)
+            {
+                MessageBox.Show(cmb.Tag + " Should be selected", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                cmb.Focus();
+                chk = false;
+            }
+            return chk;
+
+        }
+
+
+
+    }
+}
